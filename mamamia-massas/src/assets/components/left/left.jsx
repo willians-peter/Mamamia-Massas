86e8b33d920d39cba2cde62b880846a68a473899
willians-peter <petercipriano@hotmail.com>
2025-07-31 17:22:39 -0300

    iniciando2
    concluído exercício 1
    iniciando exercício 2, falta css

---
+++ mamamia-massas/src/assets/components/left/left.jsx
@@ -0,0 +1,10 @@
+function LadoEsquerdo() {
+  return (
+    <div className="lado-esquerdo">
+      <div>Servindo massas há mais de 70 anos!</div>
+      <div>Qualidade passada por gerações!</div>
+    </div>
+  );
+}
+
+export default LadoEsquerdo;
---
+++ mamamia-massas/src/assets/components/rigth/rigth.jsx
@@ -0,0 +1,11 @@
+import fachadaImage from "./fachada.jpg";
+
+function LadoDireito() {
+  return (
+    <div className="lado-direito">
+      <img src={fachadaImage} alt="fachada do restaurante" />
+    </div>
+  );
+}
+
+export default LadoDireito;
